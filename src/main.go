913aaf2b9e5dd7d617386a3db95a4f0b3b158147
yanlele <331393627@qq.com>
2023-12-10 23:51:27 +0800

    feat: 日志初始化

--- src/main.go
+++ src/main.go
@@ -1,25 +1,39 @@
 package main
 
 import (
+	"errors"
 	log "unknwon.dev/clog/v2"
 )
 
 func init() {
-	err := log.NewConsole(
-		log.DefaultFileName,
-		0,
-		log.ConsoleConfig{Level: log.LevelTrace},
-	)
-
+	err := log.NewConsole()
 	if err != nil {
 		panic("unable to create new logger: " + err.Error())
+	}
+
+	err = log.NewFile(log.FileConfig{
+		Filename: "./logs/clog.log",
+		Level:    log.LevelTrace,
+		FileRotationConfig: log.FileRotationConfig{
+			Rotate: true,
+			Daily:  true,
+			//MaxLines: 50,
+		},
+	})
+
+	if err != nil {
+		panic("unable to create new logger with file: " + err.Error())
 	}
 }
 
 func main() {
-	log.Trace("hello %s", "world")
-	log.Info("Hello %s!", "World") // YYYY/MM/DD 12:34:56 [ INFO] Hello World!
-	log.Warn("Hello %s!", "World") // YYYY/MM/DD 12:34:56 [ WARN] Hello World!
+	err := errors.New("错误信息")
+
+	log.Trace("Hello %s!", "World")
+	log.Info("Hello %s!", "World")
+	log.Warn("Hello %s!", "World")
+	log.Error("So bad... %v", err)
+	log.Fatal("Boom! %v", err)
 
 	defer log.Stop()
 }
