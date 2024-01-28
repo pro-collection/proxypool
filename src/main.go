e87c733c7b5273fb1a8e90b6f6fc372b54c4f214
yanlele <331393627@qq.com>
2024-01-28 21:13:33 +0800

    feat： *

--- src/main.go
+++ src/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"proxypool/pkg/getter"
+	"proxypool/pkg/models"
+	"sync"
 	log "unknwon.dev/clog/v2"
 )
 
@@ -28,9 +31,39 @@ func init() {
 	}
 }
 
-func main() {
-	//getter.IP89()
-	getter.IP3306()
+type MyFunction func() []*models.IP
 
+func run() {
+	var ipList []*models.IP
+
+	var wg sync.WaitGroup
+
+	funs := []MyFunction{
+		getter.IP3306,
+		getter.IP89,
+	}
+
+	for _, fun := range funs {
+		wg.Add(1)
+
+		go func(f MyFunction) {
+			temp := f()
+
+			for _, ip := range temp {
+				ipList = append(ipList, ip)
+			}
+			wg.Done()
+		}(fun)
+	}
+
+	wg.Wait()
+
+	jsonData, _ := json.Marshal(ipList)
+
+	log.Info(string(jsonData))
+}
+
+func main() {
+	run()
 	defer log.Stop()
 }
